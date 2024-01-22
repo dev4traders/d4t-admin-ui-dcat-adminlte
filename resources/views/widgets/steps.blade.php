12c443eb9323a64954ca67b83c19cd33e1e3f332
dtikhonov <dtihonov@code-pilots.ru>
2024-01-22 09:43:33 +0300

    D4T-11: поправил отображение steps и badge-with-icon для adminlte

--- resources/views/widgets/steps.blade.php
+++ resources/views/widgets/steps.blade.php
@@ -1,96 +1,31 @@
 <style>
-    .steps-list {
-        background-color: #2E2E2E;
-        border-radius: 0.5rem!important;
-    }
-    .steps-item {
-        background-color: #2E2E2E;
-        padding-right: 50px;
-        box-sizing: border-box;
-        overflow: hidden;
-        border-radius: 0.5rem!important;
-
-        &:hover {
-            cursor: default;
-            background-color: #2E2E2E;
-        }
-    }
-    .li-wrapper {
-        z-index: 2;
-    }
-    .steps-text {
-        color: #ffffff;
-    }
-    .steps-item_active .steps-square {
-        display: block;
-    }
-    .steps-square {
-        display: none;
-        width: 100%;
-        height: auto;
-        padding-top: 100%;
-        position: absolute;
-        top: 50%;
-        right: 0;
-        border-radius: 12px;
-        transform: rotate(45deg);
-        transform-origin: top right;
-        overflow: hidden;
-    }
-    .steps-square__inner {
-        display: block;
-        background: rgb(232,60,244);
-        background: linear-gradient(79deg, rgba(232,60,244,1) 0.92%, rgba(90,198,218,1) 97.8%);
-        width: 130%;
-        height: 45%;
-        position: absolute;
-        top: -30%;
-        right: 0;
-        transform: rotate(-45deg);
-        transform-origin: top right;
-    }
-    @media screen and (max-width: 1199px) {
-        .steps-square__inner {
-            width: 140%;
-            height: 50%;
-            top: -50%;
-        }
-    }
     .steps-badge {
         display: inline-flex;
-        background: rgb(232,60,244);
-        background: linear-gradient(79deg, rgba(232,60,244,1) 0.92%, rgba(90,198,218,1) 97.8%);
         align-items: center;
         justify-content: center;
         width: 55px;
         height: 55px;
         position: relative;
         border-radius: 50%;
-        color: #ffffff;
-    }
-    .steps-badge_active {
-        background: none;
-        background-color: #ffffff;
-        color: #0a0a0a;
     }
 </style>
 <div class="steps-list list-group list-group-horizontal-md text-md-center {{ is_null($class_ext) ? '' : $class_ext }}">
     @foreach($items as $idx => $item)
         <div
             id="step_{{ $item['id'] }}"
-            class="list-group-item list-group-item-action d-flex justify-content-between steps-item {{ $idx == $active_idx ? 'steps-item_active' : '' }}"
+            class="bg-dark list-group-item list-group-item-action d-flex justify-content-between steps-item {{ $idx == $active_idx ? 'steps-item_active' : '' }}"
         >
             <div class="li-wrapper d-flex justify-content-start align-items-center">
                 @if($item['icon'])
                     {!! $item['icon'] !!}
                 @else
                     <h1 class="m-0 mr-1">
-                        <span class="steps-badge {{ $idx == $active_idx ? 'steps-badge_active' : '' }}">{{ $item['index'] }}</span>
+                        <span class="steps-badge bg-secondary text-primary {{ $idx == $active_idx ? 'steps-badge_active' : '' }}">{{ $item['index'] }}</span>
                     </h1>
                 @endif
                 <div class="list-content text-left">
-                    <h4 class="m-0 steps-text">{{ $item['title'] }}</h4>
-                    <h3 class="text-bold m-0 steps-text">{{ $item['description'] }}</h3>
+                    <h4 class="m-0 steps-text text-light">{{ $item['title'] }}</h4>
+                    <h3 class="text-bold m-0 steps-text text-light">{{ $item['description'] }}</h3>
                 </div>
             </div>
 
