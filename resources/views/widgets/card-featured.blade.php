e1bad2302a897a1348841b25eb95a01ce22291ed
dtikhonov <dtihonov@code-pilots.ru>
2024-01-23 16:57:25 +0300

    D4T-19: fixed featured-card style & stat-progress-radial style

--- resources/views/widgets/card-featured.blade.php
+++ resources/views/widgets/card-featured.blade.php
@@ -1,10 +1,16 @@
+<style>
+body.dark-mode .card-body.featured-card-body {
+    border-color: #5b6fb3!important;
+}
+</style>
+
 <div class="card {{ $full_height ? 'h-100' : '' }} {{ empty($class) ? '' : 'bg-'.$class }}" style="{{ $style }}">
     @if($header_content)
         <div class="card-header">
             {!! $header_content !!}
         </div>
     @endif
-    <div class="card-body d-flex flex-column flex-md-row justify-content-between p-0">
+    <div class="featured-card-body card-body d-flex flex-column flex-md-row justify-content-between p-0 {{ $with_border ? 'border border-1 border-bottom-0 rounded-top border-'.$border_class : '' }}">
         @if ($image)
             <div class="d-inline-flex flex-column justify-content-center align-items-center pl-2">
                 <div>
@@ -28,7 +34,7 @@
         </div>
     </div>
     @if(count($footer_links) > 0)
-        <div class="card-footer d-flex align-items-center">
+        <div class="card-footer border-top-0 d-flex align-items-center p-0" style="margin-top: -1px;">
             <div class="btn-group w-100">
                 @foreach($footer_links as $link)
                     {!! $link !!}
